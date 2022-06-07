7473e8b5f83ea1a909451f4aad32c96548a74679
alearigth <91171514+alearigth@users.noreply.github.com>
2022-06-07 11:01:08 -0500

    homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,11 +3,37 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+ /* var sum = 0
+
+  for (let i = 0; i < num.length; i++) {
+
+    sum += num[i] * Math.pow(2, num.length - 1 - i);
+    
+  }
+  return sum */
+
+  var sum = 0
+  var position = 0;
+  for (let i = num.length-1; i >= 0; i--) {
+    sum += num[i] * Math.pow(2, position)
+    position++
+    
+  }
+  return sum
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  var resultado = []
+  while(num >= 1) {
+    resultado.unshift(num % 2)
+    
+    num = Math.floor(num / 2)
+    
+  }
+    return resultado.join("");
 }
 
 
